acb7edc775086c8e8c8efb4c3e8e2ce817b1a513
Arkadiusz Cios <arkadiuszcios@outlook.com>
2026-05-14 09:51:14 +0200

    feat: add OpenAPI spec and Scalar UI

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Scalar.AspNetCore;
 using trivia_game.Application.Interfaces;
 using trivia_game.Application.Services;
 using trivia_game.Domain.Interfaces.Providers;
@@ -10,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddSignalR();
+builder.Services.AddOpenApi();
 
 builder.Services
     .AddScoped<IRoomService, RoomService>()
@@ -22,6 +24,12 @@ builder.Services.AddHttpClient<ITriviaProvider, OpenTdbClient>(client =>
     client.BaseAddress = new Uri("https://opentdb.com/"));
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference(options => options.Title = "Trivia Game API");
+}
 
 app.MapControllers();
 app.MapHub<GameHub>("/hubs/game");
